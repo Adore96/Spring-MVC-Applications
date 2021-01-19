95f2f4d689fa789606d3e574e5c8b832d7e11954
Kalhara Perera <adorekasun@gmail.com>
2021-01-20 00:10:49 +0530

    dependancies fixed

--- MVC-CRUD/src/main/java/com/adore96/bean/UserDataBean.java
+++ MVC-CRUD/src/main/java/com/adore96/bean/UserDataBean.java
@@ -10,6 +10,16 @@ public class UserDataBean {
     private String name;
     private String username;
     private String password;
+    private int telephone;
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
     private String role;
 
     public int getId() {
