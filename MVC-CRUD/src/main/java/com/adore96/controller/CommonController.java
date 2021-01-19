e868245e7c9111eaa5aa06a79f92971f5b32afc3
Kalhara Perera <adorekasun@gmail.com>
2021-01-19 21:47:44 +0530

    Update

---
+++ MVC-CRUD/src/main/java/com/adore96/controller/CommonController.java
@@ -0,0 +1,23 @@
+package com.adore96.controller;
+
+import com.adore96.service.AddService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author ON 1/19/21
+ * @project MVC-CRUD
+ */
+
+@Controller
+public class CommonController {
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("Add method called in AddController");
+
+        return "index.jsp";
+    }
+}
